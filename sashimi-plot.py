6da7940cf59e5466ecb7a372e52b48dc0edae254
Alessandra Breschi <alessandra.breschi@crg.es>
2017-04-28 18:55:00 +0200

    multiple plots (viewports)

--- sashimi-plot.py
+++ sashimi-plot.py
@@ -86,6 +86,14 @@ def read_bam(f, c):
 	return a, junctions
 
 
+def read_bam_input(f):
+	if f.endswith(".bam"):
+		bn = f.strip().split("/")[-1].strip(".bam")
+		return [(bn, f)]
+	with open(f) as openf:
+		return list(line.strip().split("\t") for line in openf)
+
+
 def prepare_for_R(a, junctions, c, m):
 
 	chr, start, end = parse_coordinates(args.coordinates)
@@ -114,40 +122,56 @@ def prepare_for_R(a, junctions, c, m):
 	return x, y, dons, accs, yd, ya, counts
 
 
+
+
+
 if __name__ == "__main__":
 
 	# Argument parsing
 	
 	parser = ArgumentParser(description='Create sashimi plot for a given genomic region')
-	parser.add_argument("-b", "--bam", type=str, help="Bam file")
+	parser.add_argument("-b", "--bam", type=str, help="Individual bam file or file with a list of bam files and ids")
 	parser.add_argument("-c", "--coordinates", type=str, help="Genomic region. Format: chr:start-end. Remember that bam coordinates are 0-based")
-	parser.add_argument("-m", "--min_coverage", type=int, default=0, 
-		help="Minimum number of reads supporting a junction to be drawn [default=0]")
+	parser.add_argument("-M", "--min_coverage", type=int, default=1, 
+		help="Minimum number of reads supporting a junction to be drawn [default=1]")
 #	parser.add_argument("-s", "--smooth", action="store_true", default=False, help="Smooth the signal histogram")
 	args = parser.parse_args()
 	
 	args.coordinates = "chrX:9609491-9612406"
 #	args.coordinates = "chrX:9609491-9610000"
-	args.bam = "/nfs/no_backup/rg/epalumbo/projects/tg/work/8b/8b0ac8705f37fd772a06ab7db89f6b/2A_m4_n10_toGenome.bam"
-	
-	a, junctions = read_bam(args.bam, args.coordinates)
-	x, y, dons, accs, yd, ya, counts = prepare_for_R(a, junctions, args.coordinates, args.min_coverage)
-	
+#	args.bam = "/nfs/no_backup/rg/epalumbo/projects/tg/work/8b/8b0ac8705f37fd772a06ab7db89f6b/2A_m4_n10_toGenome.bam"
 
+	bam_dict = {}
+	for id, bam in read_bam_input(args.bam):
+		a, junctions = read_bam(bam, args.coordinates)
+	 	bam_dict[id] = prepare_for_R(a, junctions, args.coordinates, args.min_coverage)
+	
 	print """
 	library(ggplot2)
 	library(grid)
-	d = data.frame(x=c(%(x)s), y=c(%(y)s))
-#	d[['y']] = as.double(smooth(d[['y']]))
 
-	maxheight = max(d[['y']])
+	density_list = list()
+	junction_list = list()
+	"""
+	
+	for k, v in bam_dict.iteritems():
+		x, y, dons, accs, yd, ya, counts = v
+		
+		print """
+		density_list$%(id)s = data.frame(x=c(%(x)s), y=c(%(y)s))
+		junction_list$%(id)s = data.frame(x=c(%(dons)s), xend=c(%(accs)s), y=c(%(yd)s), yend=c(%(ya)s), count=c(%(counts)s))
+		""" %({
+			"id": k,
+			'x' : ",".join(map(str, x)),
+			'y' : ",".join(map(str, y)),
+			'dons' : ",".join(map(str, dons)),
+			'accs' : ",".join(map(str, accs)),
+			'yd' : ",".join(map(str, yd)),
+			'ya' : ",".join(map(str, ya)),
+			'counts' : ",".join(map(str, counts)),
+		})
 
-	scale_lwd = function(r) {
-		lmin = 0.1
-		lmax = 4
-		return( r*(lmax-lmin)+lmin )
-	}
-
+	print """	
 	height = 4
 	base_size = 14
 	theme_set(theme_bw(base_size=base_size))
@@ -155,71 +179,85 @@ if __name__ == "__main__":
 		panel.grid = element_blank()
 	)
 
-	# Density plot
-	gp = ggplot(d) + geom_bar(aes(x, y), position='identity', stat='identity')
+	pdf('%s', h=height, w=10)
+	grid.newpage()
+	pushViewport(viewport(layout = grid.layout(length(density_list), 1)))
 
-	junctions = data.frame(x=c(%(dons)s), xend=c(%(accs)s), y=c(%(yd)s), yend=c(%(ya)s), count=c(%(counts)s))
-	j_tot_counts = sum(junctions[['count']])
-
-	for (i in 1:nrow(junctions)) {
-
-		j = as.numeric(junctions[i,])
-
-		# Find intron midpoint 
-		xmid = round(mean(j[1:2]), 1)
-		ymid = max(j[3:4]) * 1.1
-
-		# Thickness of the arch
-		lwd = scale_lwd(j[5]/j_tot_counts)
-
-		curve_par = gpar(lwd=lwd)
-
-		# Choose position of the arch (top or bottom)
-		nss = length(match(j[1], junctions[,1]))
-		if (nss%%%%2 == 0) {  #bottom
-			ymid = -0.4 * maxheight
-			# Draw the archs
-			# Left
-			curve = xsplineGrob(x=c(0, 0, 1, 1), y=c(1, 0, 0, 0), shape=1, gp=curve_par)
-			gp = gp + annotation_custom(grob = curve, j[1], xmid, 0, ymid)
-			# Right
-			curve = xsplineGrob(x=c(1, 1, 0, 0), y=c(1, 0, 0, 0), shape=1, gp=curve_par)
-			gp = gp + annotation_custom(grob = curve, xmid, j[2], 0, ymid)
-		} 
-
-		if (nss%%%%2 != 0) {  #top
-			# Draw the archs
-			# Left
-			curve = xsplineGrob(x=c(0, 0, 1, 1), y=c(0, 1, 1, 1), shape=1, gp=curve_par)
-			gp = gp + annotation_custom(grob = curve, j[1], xmid, j[3], ymid)
-			# Right
-			curve = xsplineGrob(x=c(1, 1, 0, 0), y=c(0, 1, 1, 1), shape=1, gp=curve_par)
-			gp = gp + annotation_custom(grob = curve, xmid, j[2], j[4], ymid)
+	for (bam_index in 1:length(density_list)) {
 	
-			gp = gp + annotate("label", x = xmid, y = ymid, label = j[5], 
-				vjust=0.5, hjust=0.5, label.padding=unit(0.01, "lines"), 
-				label.size=NA, size=(base_size*0.352777778)*0.6
-			)
+		id = names(density_list)[bam_index]
+		d = density_list[[id]]
+		junctions = junction_list[[id]]
+	
+		maxheight = max(d[['y']])
+	
+		scale_lwd = function(r) {
+			lmin = 0.1
+			lmax = 4
+			return( r*(lmax-lmin)+lmin )
 		}
-
-#		gp = gp + annotation_custom(grob = rectGrob(x=0, y=0, gp=gpar(col="red"), just=c("left","bottom")), xmid, j[2], j[4], ymid)
-#		gp = gp + annotation_custom(grob = rectGrob(x=0, y=0, gp=gpar(col="green"), just=c("left","bottom")), j[1], xmid, j[3], ymid)
-
+	
+	
+		# Density plot
+		gp = ggplot(d) + geom_bar(aes(x, y), position='identity', stat='identity')
+	
+		j_tot_counts = sum(junctions[['count']])
+	
+		for (i in 1:nrow(junctions)) {
+	
+			j = as.numeric(junctions[i,])
+	
+			# Find intron midpoint 
+			xmid = round(mean(j[1:2]), 1)
+			ymid = max(j[3:4]) * 1.1
+	
+			# Thickness of the arch
+			lwd = scale_lwd(j[5]/j_tot_counts)
+	
+			curve_par = gpar(lwd=lwd)
+	
+			# Choose position of the arch (top or bottom)
+			nss = length(match(j[1], junctions[,1]))
+			if (nss%%%%2 == 0) {  #bottom
+				ymid = -0.4 * maxheight
+				# Draw the archs
+				# Left
+				curve = xsplineGrob(x=c(0, 0, 1, 1), y=c(1, 0, 0, 0), shape=1, gp=curve_par)
+				gp = gp + annotation_custom(grob = curve, j[1], xmid, 0, ymid)
+				# Right
+				curve = xsplineGrob(x=c(1, 1, 0, 0), y=c(1, 0, 0, 0), shape=1, gp=curve_par)
+				gp = gp + annotation_custom(grob = curve, xmid, j[2], 0, ymid)
+			} 
+	
+			if (nss%%%%2 != 0) {  #top
+				# Draw the archs
+				# Left
+				curve = xsplineGrob(x=c(0, 0, 1, 1), y=c(0, 1, 1, 1), shape=1, gp=curve_par)
+				gp = gp + annotation_custom(grob = curve, j[1], xmid, j[3], ymid)
+				# Right
+				curve = xsplineGrob(x=c(1, 1, 0, 0), y=c(0, 1, 1, 1), shape=1, gp=curve_par)
+				gp = gp + annotation_custom(grob = curve, xmid, j[2], j[4], ymid)
+		
+				gp = gp + annotate("label", x = xmid, y = ymid, label = j[5], 
+					vjust=0.5, hjust=0.5, label.padding=unit(0.01, "lines"), 
+					label.size=NA, size=(base_size*0.352777778)*0.6
+				)
+			}
+	
+	#		gp = gp + annotation_custom(grob = rectGrob(x=0, y=0, gp=gpar(col="red"), just=c("left","bottom")), xmid, j[2], j[4], ymid)
+	#		gp = gp + annotation_custom(grob = rectGrob(x=0, y=0, gp=gpar(col="green"), just=c("left","bottom")), j[1], xmid, j[3], ymid)
+	
+	
+		}
+		print(gp, vp=viewport(layout.pos.row = bam_index, layout.pos.col = 1))
 
 	}
 
+	
 
-	ggsave('%(out)s', h=height, w=10)
-	""" %({
-		'x' : ",".join(map(str, x)),
-		'y' : ",".join(map(str, y)),
-		'dons' : ",".join(map(str, dons)),
-		'accs' : ",".join(map(str, accs)),
-		'yd' : ",".join(map(str, yd)),
-		'ya' : ",".join(map(str, ya)),
-		'counts' : ",".join(map(str, counts)),
-		'out' : "tmp.pdf",
-		})
+	dev.off()
+	""" %("tmp.pdf")
+
 	exit()
 
 
