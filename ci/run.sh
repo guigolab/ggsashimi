bdf3818081849b4e1d702dddfca8a2d0af6e779a
Emilio Palumbo <emiliopalumbo@gmail.com>
2020-07-10 09:02:48 +0200

    Update ci script with PDF checksum

--- ci/run.sh
+++ ci/run.sh
@@ -3,14 +3,14 @@ set -e
 set -u
 
 # checksums
-sashimi_md5="ff7d755c26673831db3fd0dd224b8bc0"
-sashimi_anno_md5="e817cbf59608d090cf8292290dadc1cb"
+sashimi_md5="b9eae97d095ab04724c521a5ba1edd5b"
+sashimi_anno_md5="38a99516895dc3c1ab58aafbea845a4c"
 
 fail() {
     echo ${1-""} >&2 && exit 1
 }
 
-export GGSASHIMI_DEBUG=1
+# export GGSASHIMI_DEBUG=1
 
 modes=( sashimi sashimi_anno )
 
@@ -18,7 +18,7 @@ anno=""
 for m in ${modes[@]}; do
     [[ $m == "sashimi_anno" ]] && anno="-g examples/annotation.gtf"
     ./sashimi-plot.py $anno -b examples/input_bams.tsv -c chr10:27040584-27048100
-    md5=$(md5sum R_script | awk '$0=$1')
+    md5=$(sed '/^\/\(.\+Date\|Producer\)/d' sashimi.pdf | md5sum | awk '$0=$1')
     [[ $md5 == $(eval 'echo $'$m'_md5') ]] || fail "== Wrong checksum for $m mode: $md5"
 done
 
