4b936aa9d33483d6eca951afe604a266295894db
Emilio Palumbo <emiliopalumbo@gmail.com>
2021-01-21 15:38:22 +0100

    Add more versions of R and ggplot2 to TravisCI config - #54

--- ci/run.sh
+++ ci/run.sh
@@ -2,27 +2,61 @@
 set -e
 set -u
 
-# checksums
-sashimi_md5="b8ee3a415a80030ce92de15f40054a40"
-sashimi_anno_md5="131d3ad1bb27c91693c342fbc0667d48"
-sashimi_color_md5="41fee9e7e51a13a09c6d068cc6cb93cf"
-sashimi_aggr_md5="50674d8812194bbb8f8f8ddab9238b3d"
-
 fail() {
     echo ${1-""} >&2 && exit 1
 }
 
-# export GGSASHIMI_DEBUG=1
+# define global variables
+bams="examples/input_bams.tsv"
+region="chr10:27040584-27048100"
 
-modes=( sashimi sashimi_anno sashimi_color sashimi_aggr )
+modes=(
+    sashimi
+    sashimi_anno
+    sashimi_color
+    sashimi_aggr
+)
 
-for m in ${modes[@]}; do
-    [[ $m == "sashimi_anno" ]] && anno="-g examples/annotation.gtf" || anno=""
-    [[ $m == "sashimi_color" ]] && color="-C 3" || color=""
-    [[ $m == "sashimi_aggr" ]] && aggr="-C 3 -O 3 -A mean_j" || aggr=""
-    ./sashimi-plot.py $anno -b examples/input_bams.tsv -c chr10:27040584-27048100 $color $aggr
+for mode in ${modes[@]}; do
+    anno=""
+    color=""
+    aggr=""
+    case $mode in
+    sashimi_anno)
+        sashimi_md5=(
+            "131d3ad1bb27c91693c342fbc0667d48" # R 3.3.2 - ggplot2 2.2.1
+            "e7f72a5d373c93b4b0cbfa4976e9bd15" # R 3.6.3 - ggplot2 3.3.3
+            "5617a83e8b3ba6d7478c40fcff99f5d3" # R 4.0.3 - ggplot2 3.3.3
+        )
+        anno="-g examples/annotation.gtf"
+        ;;
+    sashimi_color)
+        sashimi_md5=(
+            "41fee9e7e51a13a09c6d068cc6cb93cf" # R 3.3.2 - ggplot2 2.2.1
+            "bb186bcd74674e7c8027e0dcfe2eb1fe" # R 3.6.3 - ggplot2 3.3.3
+            "2606cf532430534c15cfc3a5d0db3620" # R 4.0.3 - ggplot2 3.3.3
+        )
+        color="-C 3"
+        ;;
+    sashimi_aggr)
+        sashimi_md5=(
+            "50674d8812194bbb8f8f8ddab9238b3d" # R 3.3.2 - ggplot2 2.2.1
+            "5a0342a6093284ec0b9f85b01acb72e4" # R 3.6.3 - ggplot2 3.3.3
+            "69b2a3f5028b1d9d0675ca98967cf353" # R 4.0.3 - ggplot2 3.3.3
+        )
+        aggr="-C 3 -O 3 -A mean_j"
+        ;;
+    *)
+        sashimi_md5=(
+            "b8ee3a415a80030ce92de15f40054a40" # R 3.3.2 - ggplot2 2.2.1
+            "567d245fcd2d3d02288976e18bdee6b0" # R 3.6.3 - ggplot2 3.3.3
+            "62d039688abd9f082b44abf98f313ae2" # R 4.0.3 - ggplot2 3.3.3
+        )
+        ;;
+    esac
+    ./sashimi-plot.py $anno -b $bams -c $region $color $aggr
     md5=$(sed '/^\/\(.\+Date\|Producer\)/d' sashimi.pdf | md5sum | awk '$0=$1')
-    [[ $md5 == $(eval 'echo $'$m'_md5') ]] || fail "== Wrong checksum for $m mode: $md5"
+    [[ " ${sashimi_md5[@]} " =~ " ${md5} " ]] || fail "== Wrong checksum for $mode mode: $md5"
 done
 
 echo "== All checksums match"
